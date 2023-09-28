bf0bd13ec1ef1cfdf7d9f97554c69d682de35b23
Joy Mickle <129700768+joymickle74@users.noreply.github.com>
2023-09-27 19:54:49 -0700

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,7 @@
+d3.csv("Joy_M_Data_Request_2.csv", function(data) {
+    // for (var i = 0; i < data.length; i++) {
+    //     console.log(data[i].Name);
+    //     console.log(data[i].Age);
+    // }
+    console.log(data)
+});
